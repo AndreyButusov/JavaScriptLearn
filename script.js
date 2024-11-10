b94662f0c29db55a4b86629a0756133214e5aa89
Andrey Butusov <agbutusov@mail.ru>
2024-11-10 23:02:30 +0300

    add sem-1 task-2

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Текст из script.js");
+
+let num = 123;
+console.log(num); //вывод переменной num
+
+let a = 5;
+a = 7;
+console.log(a);  /* вывод переменной a */
+
+let b =  1 + 2 + 3;
+console.log(b.toString());
+
