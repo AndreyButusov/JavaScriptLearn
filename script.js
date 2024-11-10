4df717fad790df8e05ea3bfc7d9d521eed1e7a71
Andrey Butusov <agbutusov@mail.ru>
2024-11-10 23:16:57 +0300

    add sem-1 task-3

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-console.log("Текст из script.js");
+// Задание 1.
+//console.log("Текст из script.js");
 
-let num = 123;
-console.log(num); //вывод переменной num
 
-let a = 5;
-a = 7;
-console.log(a);  /* вывод переменной a */
+// Задание 2.
 
-let b =  1 + 2 + 3;
-console.log(b.toString());
+// let num = 123;
+// console.log(num); //вывод переменной num
 
+// let a = 5;
+// a = 7;
+// console.log(a);  /* вывод переменной a */
+
+// let b =  1 + 2 + 3;
+// console.log(b.toString());
+
+
+
+// Задание 3.
+
+const c = 10;
+const d = 2;
+
+let sum = c + d;
+let diff = c - d;
+let multiplication = c * d;
+let division = c / d;
+
+console.log(`Сумма равна: ${sum}`);
+console.log(`Разность равна: ${diff}`);
+console.log(`Произведение равно: ${multiplication}`);
+console.log(`Частное равно: ${division}`);
+
+console.log(sum + diff + multiplication + division);
