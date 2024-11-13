5a575a24257f52d3d583934a978899e4b740f978
Andrey Butusov <agbutusov@mail.ru>
2024-11-13 21:23:48 +0300

    add hw1 task2

--- hw1/script.js
+++ hw1/script.js
@@ -1,7 +1,16 @@
 //Задача 1
 
-let celsius = 30;
-let farengeit = (9 / 5 * celsius) + 32;
+// let celsius = 30;
+// let farengeit = (9 / 5 * celsius) + 32;
 
-console.log(`Температура в градусах Цельсия: ${celsius}`);
-console.log(`Температура в градусах Фаренгейта: ${farengeit}`);
+// console.log(`Температура в градусах Цельсия: ${celsius}`);
+// console.log(`Температура в градусах Фаренгейта: ${farengeit}`);
+
+
+
+//Задача 2
+
+let name = 'Ivan';
+let admin = name;
+
+console.log('Значение переменной admin:', admin);
