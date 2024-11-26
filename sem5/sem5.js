e10c35d125deb9acb1179d3379f761a6d9a804e5
Andrey Butusov <agbutusov@mail.ru>
2024-11-26 22:57:28 +0300

    add sem5 task 7

--- sem5/sem5.js
+++ sem5/sem5.js
@@ -81,12 +81,59 @@
 // Переберите этот объект циклом и возведите каждый элемент
 // этого объекта в квадрат
 
-const obj = {
-    x: 1, 
-    y: 2, 
-    z: 3
-};
+// const obj = {
+//     x: 1, 
+//     y: 2, 
+//     z: 3
+// };
 
-for (const value in obj) {
-    console.log(obj[value] ** 2); 
+// for (const value in obj) {
+//     console.log(obj[value] ** 2); 
+// }
+
+
+// Задача 7.
+// const obj = {
+// key1: {
+//     key1: 1,
+//     key2: 2,
+//     key3: 3,
+//     },
+//     key2: {
+//     key1: 4,
+//     key2: 5,
+//     key3: 6,
+//     },
+//     key3: {
+//     key1: 7,
+//     key2: 8,
+//     key3: 9,
+//     },
+//     }
+//     Найдите сумму элементов приведенного объекта.
+
+const obj = {
+    key1: {
+        key1: 1,
+        key2: 2,
+        key3: 3,
+    },
+    key2: {
+        key1: 4,
+        key2: 5,
+        key3: 6,
+    },
+    key3: {
+        key1: 7,
+        key2: 8,
+        key3: 9,
+    },
 }
+
+let sum = 0;
+for (const keyUp in obj) {
+    for (const value in obj[keyUp]) {
+    sum += obj[keyUp][value];
+    }
+}
+console.log(sum);
