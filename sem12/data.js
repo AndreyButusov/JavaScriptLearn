c64fa24d091e07e5e1c3bee65779886b698b1ad6
Andrey Butusov <agbutusov@mail.ru>
2025-01-25 23:42:52 +0300

    added sem12

---
+++ sem12/data.js
@@ -0,0 +1,22 @@
+const dataProducts = `[
+    {
+        "id": 1,
+        "url": "img/product1.png",
+        "price": 300,
+        "count": 2,
+        "color": "Red",
+        "size": "Xl",
+        "name": "MANGO PEOPLE T-SHIRT"
+    },
+    {
+        "id": 2,
+        "url": "img/product2.png",
+        "price": 52,
+        "count": 1,
+        "color": "Black",
+        "size": "Xl",
+        "name": "ELLERY X M'O CAPSULE"
+    }
+  ]`
+  
+ 
