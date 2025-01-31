7ffb740362aaf175493257cddd37d3a6e2bafe92
Andrey Butusov <agbutusov@mail.ru>
2025-01-31 22:46:59 +0300

    added hw12

---
+++ hw12/script.js
@@ -0,0 +1,81 @@
+const url = './data.json';
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+let totalPrice = 0;
+
+const list = document.querySelector('.showcase');
+const cartList = document.getElementById('cart-list');
+const cartHeader = document.querySelector('.cart_header');
+const li2cart = document.querySelector('.li2cart');
+
+const totalPriceEl = document.createElement('p');
+totalPriceEl.textContent = `Total: ${totalPrice} $`;
+cartHeader.appendChild(totalPriceEl);
+
+document.addEventListener('DOMContentLoaded', async (e) => {
+    const data = await getData(url);
+    data.forEach(element => {
+        list.insertAdjacentHTML('beforeend', `<div class="item1 ">
+            <img class="item_img" src="${element.img}" alt="${element.title}">
+            <div class="item_text">
+                <h2 class="product_name">${element.title}</h2>
+                <p class="descriprion">${element.description}</p>
+                <p class="price">$${element.price}</p>
+                <button class="item_button" data-id="${element.id}">Add to Cart</button>
+            </div>
+        </div>`)
+    });
+})
+
+async function addToCart(productId) {
+    const data = await getData(url);
+    const product = data.find(item => item.id === productId);
+    if (product) {
+        cartList.insertAdjacentHTML('beforeend', `
+            <li class="cartlist__item">
+
+                <img src="${product.img}" alt="${product.title}" width="100" height="100">
+                <button class="delete">
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467833C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z" fill="#575757" />
+                    </svg>
+                </button>
+
+                <h3 class="product_name">${product.title}</h3>
+                <p class="price">Price: $${product.price}</p>
+                
+            </li>
+        `);
+        totalPrice += Number(product.price);
+        totalPriceEl.textContent = `Total: ${totalPrice} $`;
+    }
+    
+}
+
+cartList.addEventListener('click', (e) => {
+    if(e.target.closest('.delete')) {
+        const item = e.target.closest('.cartlist__item');
+        if (item) {
+            item.remove();
+            // totalPrice -= Number(product.price);
+            // totalPriceEl.textContent = `Total: ${totalPrice} $`;
+        }
+    }
+})
+
+document.addEventListener('click', (event) => {
+    if (event.target &&
+        event.target.classList.contains('item_button')) {
+        const productId =
+            parseInt(event.target.getAttribute('data-id'), 10);
+        addToCart(productId);    
+    }
+});
